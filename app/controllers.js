02eea03ddc27d003f487c9c69e9b5dc2795d6227
justathoughtor2 <thoughthaven@gmail.com>
2015-03-25 22:20:22 -0400

    Add correct video linking

--- app/controllers.js
+++ app/controllers.js
@@ -2,9 +2,13 @@ var SanctuaryControllers = angular.module('SanctuaryControllers', []);
 
 SanctuaryControllers
   .controller('YoutubeListController',
-    ['$scope', '$http', function($scope, $http) {
+    ['$scope', '$http', '$sce', function($scope, $http, $sce) {
       $scope.feedurl = "http://gdata.youtube.com/feeds/users/enigmaticheart666/uploads?alt=json-in-script&callback=JSON_CALLBACK";
       $http.jsonp($scope.feedurl).success(function(data) {
         $scope.results = data.feed.entry;
+        $scope.resultLinks = new Array($scope.results.length);
+        $scope.results.forEach(function(result, index) {
+          $scope.resultLinks[index] = $sce.trustAsResourceUrl(result.id.$t.replace("http://gdata.youtube.com/feeds/videos", "http://www.youtube.com/embed"));
+        });
       });
     }]);
