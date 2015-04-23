61f61966012d057f560706adf244be5b9f65ecd5
justathoughtor2 <thoughthaven@gmail.com>
2015-04-23 13:04:36 -0400

    Fixes

--- app/controllers.js
+++ app/controllers.js
@@ -6,10 +6,10 @@ SanctuaryControllers
       $scope.feedurl = "http://gdata.youtube.com/feeds/users/UCj7Gf071ORhdVrrVUH9ObdA/uploads?alt=json-in-script&callback=JSON_CALLBACK";
       $http.jsonp($scope.feedurl).success(function(data) {
         $scope.results = data.feed.entry;
-        $scope.resultLinks = new Array($scope.results.length);
-        $scope.results.forEach(function(result, index) {
-          $scope.resultLinks[index] = $sce.trustAsResourceUrl(result.id.$t.replace("http://gdata.youtube.com/feeds/videos", "http://www.youtube.com/embed"));
-        });
+        $scope.resultLinks = new Array($scope.results.length - 1);
+        for(var i = 1; i < $scope.results.length; i++) {
+          $scope.resultLinks[i-1] = $sce.trustAsResourceUrl($scope.results[i].id.$t.replace("http://gdata.youtube.com/feeds/videos", "http://www.youtube.com/embed"));
+        }
         $scope.$on('test', function(ngRepeatFinishedEvent) {
           $('.bxslider').bxSlider();
         });
